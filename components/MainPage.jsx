3603514d9ce7bf8584f0e29b95c9c24cb0e7fbcb
Rahul Yedida <yrahul3910@gmail.com>
2018-04-19 12:28:49 +0530

    Added help information in the main page

--- components/MainPage.jsx
+++ components/MainPage.jsx
@@ -6,11 +6,42 @@ class MainPage extends React.Component {
         return (
             <React.Fragment>
                 <SideMenu />
-                <div style={{ marginLeft: "25%", marginTop: "20%" }}>
-                    <p>
-                        Welcome to the PESIT South Campus result scraper. Click the Individual tab for
-                        individual results, and the Batch tab for multiple results at once.
-                    </p>
+                <div style={{ marginLeft: "25vw", marginTop: "10vh", marginRight: "10vw" }}>
+                    <h3>
+                        Welcome to the PESIT South Campus result scraper
+                    </h3>
+                    <div style={{ marginTop: "10vh" }}>
+                        <span style={{ borderWidth: "2px", borderRadius: "50%",
+                            padding: "5px 9px", borderStyle: "solid", marginRight: "15px"}}>
+                            1
+                        </span>
+                        <span><strong>Fetching individual results</strong></span>
+                        <p style={{ marginTop: "15px", marginLeft: "46px" }}>Go to the <em>Individual Results</em> tab, enter the USN and the
+                        semester for the student. The results will be displayed in a table,
+                        along with the GPA.</p>
+                    </div>
+                    <div style={{ marginTop: "10vh" }}>
+                        <span style={{ borderWidth: "2px", borderRadius: "50%",
+                            padding: "5px 9px", borderStyle: "solid", marginRight: "15px"}}>
+                            2
+                        </span>
+                        <span><strong>Fetching batch results</strong></span>
+                        <p style={{ marginTop: "15px", marginLeft: "46px" }}>Go to the <em>Batch Result</em> tab, enter the USN range and the
+                        semester for the students. The results will be displayed in pie charts, with
+                        statistics for each subject. Please save the page if you wish to keep the charts.
+                        There is also an option to generate a printable report at the bottom of the page.</p>
+                    </div>
+                    <div style={{ marginTop: "10vh" }}>
+                        <span style={{ borderWidth: "2px", borderRadius: "50%",
+                            padding: "5px 9px", borderStyle: "solid", marginRight: "15px"}}>
+                            3
+                        </span>
+                        <span><strong>Updating database results</strong></span>
+                        <p style={{ marginTop: "15px", marginLeft: "46px" }}>Go to the <em>Update Reval Results</em> tab, enter the USN range and the
+                        semester for the student. The results of updating the database are shown in
+                        a table. Note that to update the database, the USN results must be scraped
+                        first, using either individual or batch results.</p>
+                    </div>
                 </div>
             </React.Fragment>
         );
